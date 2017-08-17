c25a5eb6257557a11595ea11fcbe892a835b6666
Steve Tingiris <steve@dabblelab.com>
2017-08-17 05:44:49 -0400

    simplify hello world example

--- 01-hello-world/src/index.js
+++ 01-hello-world/src/index.js
@@ -7,11 +7,7 @@ exports.handler = function(event, context, callback){
 };
 
 var handlers = {
-
   'LaunchRequest': function () {
-    this.emit(':ask', 'Hello World!', 'I would ask a question if this skill was completed.');
-  },
-  'Unhandled': function (){
-    this.emit(':tell', 'Sorry, I dont understand.');
+    this.emit(':tell', 'Hello World!');
   }
 };
